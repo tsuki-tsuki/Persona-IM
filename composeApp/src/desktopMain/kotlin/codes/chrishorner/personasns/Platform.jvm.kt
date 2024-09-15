2aae891ffb7e8b67af6dd7b59cd37038c622ff66
Jose <gdysay@gmail.com>
2024-09-15 22:40:38 +0900

    Remove unnecessary sample implementation

--- composeApp/src/desktopMain/kotlin/codes/chrishorner/personasns/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package stream.swiftmoon.personasns
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
--- composeApp/src/wasmJsMain/kotlin/codes/chrishorner/personasns/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package stream.swiftmoon.personasns
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
