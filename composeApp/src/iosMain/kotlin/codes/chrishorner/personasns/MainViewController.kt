31ca00adddb4a2283a9a9ca8dd44ba380e27157a
Jose <gdysay@gmail.com>
2024-09-18 02:03:56 +0900

    Fix formatting

--- composeApp/src/iosMain/kotlin/codes/chrishorner/personasns/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/codes/chrishorner/personasns/MainViewController.kt
@@ -2,4 +2,5 @@ package codes.chrishorner.personasns
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+@Suppress("unused", "FunctionName")
 fun MainViewController() = ComposeUIViewController { App() }
