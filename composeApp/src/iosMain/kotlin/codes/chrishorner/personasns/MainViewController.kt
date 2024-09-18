957876a271ce57172784b3818275ff03b7f92790
Jose <gdysay@gmail.com>
2024-09-19 01:18:35 +0900

    Configure iOS Compose canvas drawing
    Prefer transparent canvas for easier background color management
    
    Also remove unnecessary methods and copyright

--- composeApp/src/iosMain/kotlin/codes/chrishorner/personasns/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/codes/chrishorner/personasns/MainViewController.kt
@@ -1,6 +1,8 @@
 package codes.chrishorner.personasns
 
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.ui.window.ComposeUIViewController
 
+@OptIn(ExperimentalComposeApi::class)
 @Suppress("unused", "FunctionName")
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = { opaque = false }) { App() }
