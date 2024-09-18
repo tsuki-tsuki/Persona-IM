3d9304c5037c8d5d5a53e49188385a6917df6a36
Jose <gdysay@gmail.com>
2024-09-18 21:34:18 +0900

    Update RootContainer Desktop implementation
    Create custom DecorView to replace normal WindowDecorView for customization

--- composeApp/src/desktopMain/kotlin/codes/chrishorner/personasns/main.kt
+++ composeApp/src/desktopMain/kotlin/codes/chrishorner/personasns/main.kt
@@ -1,7 +1,10 @@
 package codes.chrishorner.personasns
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import personasns.composeapp.generated.resources.Res
@@ -9,11 +12,22 @@ import personasns.composeapp.generated.resources.app_icon
 import personasns.composeapp.generated.resources.app_name
 
 fun main() = application {
+    val state = rememberWindowState()
+
     Window(
         onCloseRequest = ::exitApplication,
         title = stringResource(Res.string.app_name),
-        icon = painterResource(Res.drawable.app_icon)
+        icon = painterResource(Res.drawable.app_icon),
+        undecorated = true,
+        transparent = true,
+        state = state,
     ) {
-        App()
+        DecorWindow(
+            modifier = Modifier.fillMaxSize(),
+            onClickMinimize = { state.isMinimized = true },
+            onClickClose = ::exitApplication,
+        ) {
+            App()
+        }
     }
 }
